50080bb7f6f44a0ffdd1c308bf12028c6e0fff56
petrus <piotr.dankowski@autea.eu>
2019-03-04 00:51:23 +0100

    add parameters validation mixin

--- exams_app/exams/views.py
+++ exams_app/exams/views.py
@@ -1,19 +1,44 @@
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.exceptions import APIException
 from rest_framework.permissions import IsAuthenticated
 
+from exams_app.exams.exceptions import InvalidParamError
 from exams_app.exams.serializers import ExamSerializer
 
 
-class ExamManagementView(viewsets.ModelViewSet):
+class ParamValidatorMixin(object):
+    '''valid_definitins should be dict {param_name: type}'''
+
+    valid_definitions = None
+
+    def valid_params(self, actual_params):
+        if not self.valid_definitions:
+            raise APIException('Validation not definied')
+        valid_names = [name for name, type in self.valid_definitions.items()]
+        actual_names = actual_params.keys()
+        if len(set(actual_names) - set(valid_names)) > 0:
+            raise InvalidParamError(f'Valid params {valid_names} , given params {actual_names}')
+        for key, value in actual_params.items():
+            try:
+                self.valid_definitions.get(key)(value)
+            except:
+                raise InvalidParamError(f'Invalid type param. {key}, should be {self.valid_definitions.get(key)}')
+
+
+class ExamManagementView(viewsets.ModelViewSet, ParamValidatorMixin):
     permission_classes = (IsAuthenticated,)
     authentication_classes = (BasicAuthentication,)
     serializer_class = ExamSerializer
+    valid_definitions = {
+        'test': int
+    }
 
     def get_queryset(self):
         pass
 
     def list(self, request, **kwargs):
+        self.valid_params(request.GET.dict())
         pass
 
     def create(self, request, *args, **kwargs):
