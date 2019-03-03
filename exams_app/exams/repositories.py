3b8b3673af6e98a886ca6bb10ce85362c30ae328
petrus <piotr.dankowski@autea.eu>
2019-03-04 00:00:13 +0100

    add integration tests for repositories minor fixes according to tests setup test database

--- exams_app/exams/repositories.py
+++ exams_app/exams/repositories.py
@@ -1,6 +1,7 @@
 import abc
 
 from django.core.exceptions import ObjectDoesNotExist
+from collections import abc as col_abc
 
 
 class AbstractReposository(metaclass=abc.ABCMeta):
@@ -18,7 +19,7 @@ class AbstractReposository(metaclass=abc.ABCMeta):
         pass
 
     @abc.abstractmethod
-    def find_by_id(self):
+    def find_by_id(self, model_id):
         pass
 
     @abc.abstractmethod
@@ -38,10 +39,10 @@ class BaseRepository(AbstractReposository):
         raise NotImplementedError('Cannot update, use specified repository')
 
     def delete_model(self, model_id):
-        self._model_class.objects.get(model_id).delete()
+        self._model_class.objects.get(id=model_id).delete()
 
-    def find_by_id(self):
-        return self._model_class.objects.get(id)
+    def find_by_id(self, model_id):
+        return self._model_class.objects.get(id=model_id)
 
     def filter(self, **kwargs):
         return self._model_class.objects.filter(**kwargs)
@@ -52,18 +53,22 @@ class ExamRepository(BaseRepository):
     def crate_model(self, questions, user):
         exam = self._model_class()
         exam.save()
-        exam.questions = questions
+        exam.questions.set([questions] if not isinstance(questions, col_abc.Iterable) else questions)
         exam.owner = user
         exam.save()
+        return exam
 
     def update_model(self, model_id, **kwargs):
         try:
-            exam = self._model_class.objects.get(model_id)
-            exam.questions = kwargs.get('questions')
+            exam = self._model_class.objects.get(id=model_id)
+
+            exam.questions.set([kwargs.get('questions')] if not isinstance(kwargs.get('questions'), col_abc.Iterable) else kwargs.get('questions'))
+            exam.save()
         except ObjectDoesNotExist as e:
             exam = self._model_class()
             exam.save()
-            exam.questions = kwargs.get('questions')
+            exam.questions.set([kwargs.get('questions')] if not isinstance(kwargs.get('questions'), col_abc.Iterable) else kwargs.get('questions'))
             exam.owner = kwargs.get('user')
             exam.save()
+        return exam
 
--- exams_app/settings.py
+++ exams_app/settings.py
@@ -80,7 +80,8 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT')
+        'PORT': os.environ.get('DB_PORT'),
+        'TEST': {'NAME': 'test_db'}
     }
 }
 
